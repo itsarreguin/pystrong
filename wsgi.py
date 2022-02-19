89dc4f8e81e37c9e24583ff80f69e208809ed90f
Brandon Arreguin <arreguin.tech@gmail.com>
2022-02-19 15:40:08 -0600

    Modified files

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,14 @@
 from app import pystrong
+from flask import render_template, redirect
+from flask import url_for, flash
 
 app = pystrong()
 
 
 @app.route('/')
 def hello():
-    return 'Hello Pystrong'
+    render = render_template('index.html')
+    return render
 
 
 if __name__ == '__main__':
