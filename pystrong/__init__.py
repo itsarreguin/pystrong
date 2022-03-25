b59889e89d1023aa92f1d63666e01fcfd7aebba7
Brandon Arreguin <arreguin.tech@gmail.com>
2022-03-25 10:35:32 -0600

    New app structure

--- pystrong/__init__.py
+++ pystrong/__init__.py
@@ -1,28 +1,16 @@
 from flask import Flask
 from flask import render_template, url_for
 
-app = Flask(__name__)
-app.config.update(env="development")
+
+def create_app():
+    
+    app = Flask(__name__)
+    app.config.update(env="development")
 
 
-"""
-Import and register blue prints
-"""
-from pystrong.views import general
-app.register_blueprint(general.mod)
-
-
-
-"""
-Manage page errors
-"""
-@app.errorhandler(404)
-def not_found(error):
-    render = render_template('404.html', error=error)
-    return render
-
-
-@app.errorhandler(500)
-def server_error(error):
-    render = render_template('500.html', error=error)
-    return render
+    # General blue prints
+    from pystrong.views import general
+    app.register_blueprint(general.mod)
+    
+    
+    return app
